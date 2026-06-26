2f961e52274cb456b4251852dfdbff0842b379df
runner <runner@hmpps-github-actions-runner-655bb86fc5-n2kh2>
2026-06-26 11:19:54 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/justicedataagentclientapi/JusticeDataAgentClientApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/justicedataagentclientapi/JusticeDataAgentClientApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.justicedataagentclientapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class JusticeDataAgentClientApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<JusticeDataAgentClientApi>(*args)
 }
