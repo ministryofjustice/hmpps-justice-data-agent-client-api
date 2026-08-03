065bfd5b03bd7737a05ac7244115fcab0fd3e75e
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2026-08-03 10:11:13 +0100

    adding endpoint for updating prompt request

--- src/main/kotlin/uk/gov/justice/digital/hmpps/justicedataagentclientapi/resource/PromptResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/justicedataagentclientapi/resource/PromptResource.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,6 +27,15 @@ class PromptResource(
   suspend fun createPrompt(@RequestBody promptRequest: PromptRequest): ResponseEntity<PromptResponse> {
     val prompt = promptService.savePrompt(promptRequest)
     return ResponseEntity.status(HttpStatus.CREATED).body(prompt)
+  }
+
+  @PutMapping("/prompts/{key}")
+  @PreAuthorize("hasAnyRole('JUSTICE_DATA_AGENT_PROMPTS')")
+  suspend fun updatePrompt(
+    @PathVariable key: String,
+    @RequestBody promptRequest: PromptRequest): ResponseEntity<PromptResponse> {
+    val prompt = promptService.updatePrompt(key, promptRequest)
+    return ResponseEntity.status(HttpStatus.OK).body(prompt)
   }
 
   @GetMapping("/prompts")
